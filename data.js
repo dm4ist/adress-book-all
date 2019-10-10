1851ff41810e9a341a6861ebcb858b4df643b5b2
Dmitriy <41679616+dm4ist@users.noreply.github.com>
2019-10-10 17:14:20 +0300

    Edit user friendly reg

--- data.js
+++ data.js
@@ -1,8 +1,8 @@
 // Надеюсь такой регулярки хватит для проверки карты (хотя понимаю что нужно 
 // проверка для двух первых цифр (Visa/MasterCard...), Проверка первых шести цифр (Банк Id) и Алгоритм Луна и т.д. 
-const formatCard = /^[0-9]{4}\s[0-9]{4}\s[0-9]{4}\s[0-9]{4}$/;
+const formatCard = /^[0-9]{16}$/;
 const formatName = /^[A-Z]{1}[a-z]+$/;
 const formatAge = /(?:\b|-)([1-9]{1,2}[0]?|100)\b/;
-const formatTel = /^\+\d\d\(\d\d\d\)[0-9]{7}$/;
+const formatTel = /^\+[0-9]{12}$/;
 const formatEmail = /^[0-9a-z.-]{1,}@[a-z0-9.-]{1,}\.[a-z]{2,4}$/i;
 const forBtn = document.getElementById('forBtn');
